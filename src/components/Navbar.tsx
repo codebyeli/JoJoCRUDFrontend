ce7671ecba32f48d2123df764964ec1495fa77a8
NotEli809 <nomadkillz007@gmail.com>
2023-07-03 00:26:05 -0400

    added image to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,8 @@ function Navbar () {
     return (
     <nav className="navbar bg-dark border-bottom border-bottom-dark" data-bs-theme="dark">
     <div className="container-fluid">
-    <a className="navbar-brand">JoJoCRUD</a>
+    <a className="navbar-brand"><img src="https://i.ibb.co/jWY439f/Giorno-Icon.png" alt="GiornoIcon" width={30} height={30} /> JoJoCRUD
+    </a>
     <a>
     <button className="navBarbutton">Show</button>
     <button className="navBarbutton">Create</button>
