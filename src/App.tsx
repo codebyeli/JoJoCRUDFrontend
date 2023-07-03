313af57244017cfc80c43c3e6d6a1975cdb33ca8
NotEli809 <nomadkillz007@gmail.com>
2023-07-03 00:41:27 -0400

    added comments with new tasks to consider

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,20 @@ import PutForm from './components/PutForm';
 import DeleteForm from './components/DeleteForm';
 import Navbar from './components/Navbar';
 
+/*
+Glossary
+
+Comments with suggestions that are done will have ✔
+Comments with suggestions that are being considered or in progress will have -
+Comments with suggestions that have been scrapped will have [x]
+*/
+
+/* 
+  - Background image for the page should be killer queen's icon patterns with pink and purple as the color pallette.
+  - Forms should be centered and have a rounded square as a background.
+  - Frontpage will be the "Help page" with the user having to click any of the options in the Navbar to do the CRUD actions.
+*/
+
 function App() {
   return (
     <>
