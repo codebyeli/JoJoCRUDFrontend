d45ee2fbdd6dade5fda85d86de88998fa0f1ba49
NotEli809 <nomadkillz007@gmail.com>
2023-07-04 12:17:00 -0400

    fix navbar to be stiky top

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 function Navbar () {
     
     return (
-    <nav className="navbar justify-content-start bg-dark border-bottom border-bottom-dark" data-bs-theme="dark">
+    <nav className="navbar sticky-top justify-content-start bg-dark border-bottom border-bottom-dark" data-bs-theme="dark">
     <Link className="navbar-brand" to="/"><img src="https://i.ibb.co/jWY439f/Giorno-Icon.png" alt="GiornoIcon" width={40} /> JoJoCRUD</Link>
     <li className="nav-item">
     <Link className="btn btn-dark" to="/get">Show</Link>
